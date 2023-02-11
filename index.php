ff68f874ee27b3b730062e416783b4c99fff969c
Deepak <deepakchoudharysl78@gmail.com>
2023-02-11 17:58:35 +0530

    Changed the language

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ChitChat</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <center>
+        <h1>ChitChat</h1>
+    </center>
+    <h4>Special Features</h4>
+    <li>Website for Smoother Chatting</li>
+    <li>Low Responce Time</li>
+    <li>Good UI</li>
+    <li>Privacy</li>
+    <li>No SignIn Required</li>
+    <center><button>Enter</button></center>
+</body>
+</html>
